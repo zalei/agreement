a524e6c7341bb2259bdc727e6c0649f2dc7845c5
Залевский Евгений <zal_ei@mail.ru>
2023-01-17 23:00:14 +0300

    Добавлена модель “Виды договоров” (agreement.contract.type). Определены права доступа на модель, представления согласно ТЗ.

--- models/models.py
+++ models/models.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api, _
 
 class AgreementContract(models.Model):
   _name = 'agreement.contract'
-  _description = 'agreement.contract'
+  _description = 'Договоры'
 
   @api.model
   def create(self, vals):
@@ -12,5 +12,26 @@ class AgreementContract(models.Model):
       vals['number'] = self.env['ir.sequence'].next_by_code('agreement.contract')
     return super().create(vals)
 
-  # number = fields.Char('Номер', default=lambda self: _('New'), copy=False, readonly=True, tracking=True)
-  number = fields.Char('Номер', default=lambda self: _('New'))
+  number = fields.Char('Номер', default=lambda self: _('New'), copy=False, readonly=True, tracking=True)
+  partner_id = fields.Many2one('res.partner', string='Partner', required=True, ondelete='restrict')
+  state = fields.Selection([('draft', 'Черновик'),
+                            ('for_approval', 'На согласовании'),
+                            ('enabled', 'Активен'),
+                            ('closed', 'Завершен')], default='draft', required=True)
+
+  start_date = fields.Date('Дата начала', required=True)
+  end_date = fields.Date('Дата завершения', required=True)
+
+  author_id = fields.Many2one('res.users', string='Автор', default=lambda self: self.env.user, required=True)
+
+
+class AgreementContractType(models.Model):
+  _name = 'agreement.contract.type'
+  _description = 'Справочник “Виды договоров”'
+  _sql_constraints = [
+        ('contract_type_name_uniq', 'UNIQUE (name)',
+         'Внимание! Такое название вида договора уже есть!'),
+    ]
+
+  name = fields.Char('Название', required=True)
+  active = fields.Boolean('Активно', default=True)
